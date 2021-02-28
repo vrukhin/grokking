4b607a3f9188eae439cab584997fd71109cc1757
Pavel Vrukhin <vrukhin@protonmail.com>
2021-02-28 22:26:41 +0300

    В задание 4.3 добавлено решение автора

--- Chapter 04/04_03_maximum.py
+++ Chapter 04/04_03_maximum.py
@@ -6,4 +6,14 @@ def maximum(nums):
     else:
         return max(nums.pop(), maximum(nums))
         
+        
 print(maximum([2,4,6]))
+
+
+def maximum2(nums):
+    if len(nums) == 2:
+        return nums[0] if nums[0] > nums[1] else nums[1]
+    sub_max = maximum2(nums[1:])
+    return nums[0] if nums[0] > sub_max else sub_max
+        
+print(maximum2([2,4,6]))
